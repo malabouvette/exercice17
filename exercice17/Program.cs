4204fba6964a61af1eb61a40f051c9103688b300
malabouvette <74193387+malabouvette@users.noreply.github.com>
2020-11-11 10:09:15 +0100

    Ajoutez des fichiers projet.

---
+++ exercice17/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaration
+            int number;
+            double numberSqrt;
+
+            // invitaion saisie nombre
+            Console.Write("Saisir un nombre = ");
+            number = int.Parse(Console.ReadLine());
+
+            // invitatiaon saisie racine carrée
+            Console.Write("Saisir la racine carrée du nombre saisi = ");
+            numberSqrt = double.Parse(Console.ReadLine());
+
+            if (numberSqrt == Math.Sqrt(number))
+            {
+                Console.WriteLine("Bravo !");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Erreur, la racine carrée de " + number + " est " + Math.Sqrt(number));
+                Console.ReadLine();
+            }
+        }
+    }
+}
